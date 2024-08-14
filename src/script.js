add9a0e1a9970cc84b05a54381c161f868f1fc38
Rupesh kumar <rkn0907@gmail.com>
2024-08-14 22:57:27 +0530

    first-commit

---
+++ src/script.js
@@ -0,0 +1,58 @@
+const aside = document.querySelector("aside");
+const asideBtn = document.querySelector(".show-contacts");
+
+asideBtn.addEventListener("click", () => {
+  aside.classList.toggle("height");
+});
+
+// headerBtn
+const headerBtn = document.querySelectorAll(".headerBtn");
+const aboutSection = document.querySelector(".About-section");
+const resumeSection = document.querySelector(".resume-section");
+const portfolioSection = document.querySelector(".portfolio-section");
+const contactsSection = document.querySelector(".contact-section");
+
+headerBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // if (headerBtn[0].innerHTML == e.target.innerHTML) {
+    //   e.target.style.color = "var(--orange)";
+    // } else {
+    //   e.target.style.color = "none";
+    // }
+    // if (headerBtn[1].innerHTML == e.target.innerHTML) {
+    //   e.target.style.color = "var(--orange)";
+    // } else {
+    //   e.target.style.color = "none";
+    // }
+    console.log(btn);
+
+    if (e.target.innerHTML == "About") {
+      aboutSection.style.display = "block";
+      resumeSection.style.display =
+        portfolioSection.style.display =
+        contactsSection.style.display =
+          "none";
+    }
+    if (e.target.innerHTML == "Resume") {
+      resumeSection.style.display = "block";
+      aboutSection.style.display =
+        portfolioSection.style.display =
+        contactsSection.style.display =
+          "none";
+    }
+    if (e.target.innerHTML == "Portfolio") {
+      portfolioSection.style.display = "block";
+      aboutSection.style.display =
+        resumeSection.style.display =
+        contactsSection.style.display =
+          "none";
+    }
+    if (e.target.innerHTML == "Contact") {
+      aboutSection.style.display =
+        resumeSection.style.display =
+        portfolioSection.style.display =
+          "none";
+      contactsSection.style.display = "block";
+    }
+  });
+});
